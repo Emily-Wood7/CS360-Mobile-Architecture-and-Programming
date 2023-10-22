f78c8ac6cae41ae932a9a7fe3dc29b3b0e6abed5
Emily-Wood7 <emily.wood7@snhu.edu>
2023-10-22 01:34:14 -0400

    Notifications Implemented

--- WoodEWeightTracker/app/src/main/java/com/cs360/woodeweighttracker/NotificationsActivity.java
+++ WoodEWeightTracker/app/src/main/java/com/cs360/woodeweighttracker/NotificationsActivity.java
@@ -1,18 +1,38 @@
 package com.cs360.woodeweighttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NotificationsActivity extends AppCompatActivity {
+
+    EditText editTextPhone;
+    Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        editTextPhone = findViewById(R.id.editTextPhone);
+        buttonSubmit = findViewById(R.id.buttonSubmitPhone);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(NotificationsActivity.this, "Notifications Enabled", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
